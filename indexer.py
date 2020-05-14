1eeb4855e95009aff0c66511c7f0beae544e0a4a
unknown <bruceknowshow@ymail.com>
2020-05-14 16:50:57 -0700

    Removed index partitioning
    - Index actually needs to be fragmented and then merged during construction. I have attempted to do this with ijson but haven't succeeded yet.

--- indexer.py
+++ indexer.py
@@ -1,89 +1,29 @@
 from bs4 import BeautifulSoup
-import nltk
 import os
 import json
 import re
 from collections import defaultdict
 import time
+import shutil
+import ijson
+import glob
 
 corpus_folder = os.path.join(os.getcwd(), 'developer')
-index_folder = os.path.join(os.getcwd(), 'Index')
+index_folder = os.path.join(os.getcwd(), 'index')
 
-if (not os.path.exists(index_folder)):
-	os.makedirs(index_folder)
-
-partition_mapfile = os.path.join(index_folder, 'map.json')
-
-PARTITION_COUNT = 100
-ACTIVE_PARTITIONS = 100
+if (os.path.exists(index_folder)):
+	shutil.rmtree(index_folder)
+os.makedirs(index_folder)
 
 MAX_DOCUMENTS = 100000
+DOCS_PER_FRAGMENT = 100000
 
 start_time = time.time()
-
-class InvertedIndex:
-	def __init__(self, partition_map={}, partitions=PARTITION_COUNT, max_active_partitions=ACTIVE_PARTITIONS):
-		self.partitions = partitions
-		self.partition_map = partition_map
-		
-		self.max_active_partitions = max_active_partitions
-		self.loaded = []
-		
-		self.index = [{} for i in range(self.partitions)]
-		
-		for i in range(PARTITION_COUNT):
-			for k in [k for k, v in self.partition_map.items() if v == i]:
-				self.index[i][k] = []
-			with open(os.path.join(index_folder, str(i) + '.json'), 'w') as fp:
-				json.dump(self.index[i], fp, indent=True)
-		
-	
-	def unload_partition(self, id):
-		if (id not in self.loaded):
-			return
-		
-		self.loaded.remove(id)
-		
-		with open(os.path.join(index_folder, str(id) + '.json'), 'w') as fp:
-			json.dump(self.index[id], fp, indent=True)
-		
-		self.index[id] = None
-	
-	def load_partition(self, id):
-		if (id in self.loaded):
-			self.loaded.remove(id)
-			self.loaded.insert(0, id)
-			return
-		
-		if (len(self.loaded) > self.max_active_partitions):
-			print('loading', id)
-			self.unload_partition(self.loaded[-1])
-		
-		self.loaded.insert(0, id)
-		
-		with open(os.path.join(index_folder, str(id) + '.json'), 'r') as fp:
-			self.index[id] = json.load(fp)
-	
-	def get_partition(self, partition):
-		self.load_partition(partition)
-		return self.index[partition]
-	
-	def set_partition(self, partition, value):
-		self.load_partition(partition)
-		self.index[partition] = value
-	
-	def __getitem__(self, key):
-		self.load_partition(self.partition_map[key])
-		return self.index[self.partition_map[key]][key]
-	
-	def __setitem__(self, key, value):
-		self.load_partition(self.partition_map[key])
-		self.index[self.partition_map[key]][key] = value
 
 def tokenize(text):
 	text = re.sub('[^A-Za-z0-9]+', ' ', text)
 	
-	return [token for token in text.lower().split() if (len(token) > 2 and (not token.isnumeric() or len(token) <= 6))]
+	return text.lower().split()
 
 def token_freqs(tokens): # returns {token: count} dict of str->int
 	ret = {}
@@ -96,115 +36,58 @@ def token_freqs(tokens): # returns {token: count} dict of str->int
 	
 	return ret
 
-def build_partition_map():
-	partition_map = {}
-	doc_count = 0
-	total_tokens = 0
-	total_docs_dict = defaultdict(int)
-	all_freqs_dict = defaultdict(int)
+def BuildIndexFragments(corpus_folder):
+	inverted_index = defaultdict(lambda: [])
+	url_dict = {}
 	
-	try:
-		with open(partition_mapfile, 'r') as fp:
-			partition_map = json.load(fp)
-		print('Found partition map file on disk, re-using', partition_mapfile, end='')
-		return partition_map
-	except FileNotFoundError:
-		pass
-	
-	for directory, subdirectories, files in os.walk(corpus_folder):
-		for file in files:
-			filename = os.path.join(directory, file)
+	frag_id = 0
+	doc_id = 0
+	for filename in glob.iglob(corpus_folder + '/**/*.json', recursive=True):
+		filetext = None
+		with open(filename, 'r') as fp:
+			filetext = fp.read()
 
-			if not filename.endswith('.json'):
-				continue
+		json_dict = json.loads(filetext)
+		soup = BeautifulSoup(json_dict['content'], 'html.parser')
+		text_tokens = tokenize(soup.text)
+		
+		freq_dict = token_freqs(text_tokens)
+		
+		for k,v in freq_dict.items():
+			inverted_index[k].append([doc_id, v])
+			url_dict[doc_id] = json_dict['url']
+		
+		if (doc_id % DOCS_PER_FRAGMENT == DOCS_PER_FRAGMENT - 1):
+			with open(os.path.join(index_folder, 'fragment' + str(frag_id) + '.json'), 'w') as fp:
+				json.dump(inverted_index, fp, indent=True)
 			
-			filetext = None
-			with open(filename, 'r') as fp:
-				filetext = fp.read()
-
-			json_dict = json.loads(filetext)
-			soup = BeautifulSoup(json_dict['content'], 'html.parser')
-			text_tokens = tokenize(soup.text)
-			
-			# total_tokens += len(text_tokens)
-			
-			for k, v in token_freqs(text_tokens).items():
-				total_docs_dict[k] += 1
-				all_freqs_dict[k] += v
-			
-			total_tokens += len(token_freqs(text_tokens).items())
-			
-			print('\rProcessing document number: ' + str(doc_count) + ', unique tokens so far: ' + str(len(all_freqs_dict)) + ', seconds: ' + str(time.time() - start_time), end='')
-			
-			doc_count += 1
-			if (doc_count > MAX_DOCUMENTS):
-				break
-		else:
-			continue
-		break
+			frag_id += 1
+			inverted_index = defaultdict(lambda: [])
+		
+		print('\rProcessing document number: ' + str(doc_id) + ', seconds: ' + str(time.time() - start_time), end='')
+		doc_id += 1
+		if (doc_id >= MAX_DOCUMENTS):
+			break
 	
-	ordered = {k: v for k, v in sorted(total_docs_dict.items(), key=lambda item: item[1], reverse=True)}
+	if (doc_id % DOCS_PER_FRAGMENT != DOCS_PER_FRAGMENT - 1):
+		with open(os.path.join(index_folder, 'fragment' + str(frag_id) + '.json'), 'w') as fp:
+			json.dump(inverted_index, fp, indent=True)
 	
-	i = 0
-	partition = 0
-	for k,v in ordered.items():
-		partition_map[k] = partition
-		i += v
-		if (i > total_tokens / PARTITION_COUNT):
-			i = 0
-			partition += 1
+	print('\nSaved ' + str(frag_id+1) + ' inverted index fragments.')
 	
-	with open(partition_mapfile, 'w') as fp:
-		json.dump(partition_map, fp, indent=True)
+	with open('url_dict.json', 'w') as fp:
+		json.dump(url_dict, fp, indent=True)
+	print('\nSaved url_dict.json')
 	
-	return partition_map
+	return inverted_index, url_dict
 
 def main():
-	print('Building partition map')
-	partition_map = build_partition_map()
-	print('\nFinished building partition map.\n')
+	inverted_index, url_dict = BuildIndexFragments(corpus_folder)
 	
-	print('Building inverted index.')
-	
-	myindex = InvertedIndex(partition_map=partition_map)
-	
-	doc_count = 0
-	for directory, subdirectories, files in os.walk(corpus_folder):
-		for file in files:
-			filename = os.path.join(directory, file)
-
-			if not filename.endswith('.json'):
-				continue
-			
-			filetext = None
-			with open(filename, 'r') as fp:
-				filetext = fp.read()
-
-			json_dict = json.loads(filetext)
-			soup = BeautifulSoup(json_dict['content'], 'html.parser')
-			text_tokens = tokenize(soup.text)
-			
-			freq_dict = token_freqs(text_tokens)
-			
-			for k,v in freq_dict.items():
-				id = partition_map[k]
-				store_object = [doc_count, json_dict['url'], v]
-				myindex[k].append(store_object)
-			
-			print('\rProcessing document number: ' + str(doc_count) + ', seconds: ' + str(time.time() - start_time), end='')
-			doc_count += 1
-			if (doc_count > MAX_DOCUMENTS):
-				break
-		else:
-			continue
-		break
-	
-	print('\nFinished building inverted index.')
-	
-	for i in range(PARTITION_COUNT):
-		myindex.unload_partition(i)
-	
-	print('Saved all index partitions to ' + index_folder)
-	
+	# frag_list = []
+	# for filename in glob.iglob(index_folder + '/*.json'):
+	# 	frag_list.append(ijson.items(open(filename, 'r'), ''))
+	# 
+	# print(frag_list[0].items)
 
 main()
